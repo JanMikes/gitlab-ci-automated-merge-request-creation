ab0fb3f8b34a687524c586d42c913d59d2ee35b0
Jan Mikeš <j.mikes@me.com>
2021-10-07 19:13:32 +0200

    Display parent branch

--- count-branch-merge-requests.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-merge_requests_response=$(curl --header "Authorization:Bearer $PERSONAL_ACCESS_TOKEN" "https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/merge_requests?state=opened&source_branch=$CI_COMMIT_BRANCH")
-
-echo $merge_requests_response | jq 'length'
