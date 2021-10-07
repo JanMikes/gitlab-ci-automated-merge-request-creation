225fb26b3517a092f0713a104abcb3d92ef86691
Jan Mikeš <j.mikes@me.com>
2021-10-07 18:44:25 +0200

    Debugging

--- count-branch-merge-requests.sh
+++ count-branch-merge-requests.sh
@@ -2,4 +2,5 @@
 
 merge_requests_response=$(curl --header "Authorization:Bearer $PERSONAL_ACCESS_TOKEN" "https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/merge_requests?state=open&source_branch=$CI_COMMIT_BRANCH")
 
+echo $merge_requests_response
 echo $merge_requests_response | jq 'length'
