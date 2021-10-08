3e78cff19a5c64d7cd324e64cb515df5771d149c
Jan Mikeš <j.mikes@me.com>
2021-10-08 10:30:52 +0200

    Skip pipeline for default branch

--- open-merge-request.sh
+++ open-merge-request.sh
@@ -24,5 +24,3 @@ echo $created_merge_request_response
 merge_request_url=$(echo $created_merge_request_response | tr '\r\n' ' ' | jq '.web_url' | tr -d '"')
 
 echo "Created MR link: ${merge_request_url}"
-
-if [ -z "$merge_request_url" ]; then exit 1; fi;
