853cc411e8c73d30fd685894d286db2e7fa3f196
Jan Mikeš <j.mikes@me.com>
2021-10-08 09:48:06 +0200

    More debug

--- open-merge-request.sh
+++ open-merge-request.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
+jq --version
+
 # Need all remote branches
 git fetch --no-tags
 
@@ -13,7 +15,7 @@ nearest_parent_branch=$(git show-branch --remote | sed "s/].*//" | grep "++ \[" 
 echo $nearest_parent_branch
 
 echo $CI_COMMIT_MESSAGE
-request_body=jq -c -n --arg title "$CI_COMMIT_MESSAGE" '{title: $title}'
+request_body=$(jq -c -r -n --arg title "$CI_COMMIT_MESSAGE" '{title: $title}')
 echo $request_body
 created_merge_request_response=$(curl -X POST --header "Authorization:Bearer ${PERSONAL_ACCESS_TOKEN}" --header "Content-Type:application/json" -d "$request_body" "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/merge_requests?source_branch=${CI_COMMIT_BRANCH}&target_branch=${nearest_parent_branch}")
 
