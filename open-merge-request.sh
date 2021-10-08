57b05f844cd3cf1dc8cc0425457d4499e62c6dd7
Jan Mikeš <j.mikes@me.com>
2021-10-08 10:06:52 +0200

    Merge branch 'testing_child_3' into 'testing_child_2' More debug See merge request cd

--- open-merge-request.sh
+++ open-merge-request.sh
@@ -14,6 +14,8 @@ echo "Nearest parent branch: ${nearest_parent_branch}"
 request_body=$(jq -c -r -n --arg title "$CI_COMMIT_MESSAGE" '{title: $title}')
 created_merge_request_response=$(curl -X POST --header "Authorization:Bearer ${PERSONAL_ACCESS_TOKEN}" --header "Content-Type:application/json" -d "${request_body}" "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/merge_requests?source_branch=${CI_COMMIT_BRANCH}&target_branch=${nearest_parent_branch}")
 
+echo $created_merge_request_response
+
 merge_request_url=$(echo $created_merge_request_response | jq '.web_url' | tr -d '"')
 
 echo "Created MR link: ${merge_request_url}"
