ce4ff2998aa50cf3437c125aa3506126ca872e15
Jan Mikeš <j.mikes@me.com>
2021-10-08 10:07:57 +0200

    Debug statements

--- open-merge-request.sh
+++ open-merge-request.sh
@@ -12,8 +12,13 @@ nearest_parent_branch=$(git show-branch --remote | sed "s/].*//" | grep "++ \[" 
 echo "Nearest parent branch: ${nearest_parent_branch}"
 
 request_body=$(jq -c -r -n --arg title "$CI_COMMIT_MESSAGE" '{title: $title}')
+
+## Debug
+echo $request_body
+
 created_merge_request_response=$(curl -X POST --header "Authorization:Bearer ${PERSONAL_ACCESS_TOKEN}" --header "Content-Type:application/json" -d "${request_body}" "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/merge_requests?source_branch=${CI_COMMIT_BRANCH}&target_branch=${nearest_parent_branch}")
 
+## Debug
 echo $created_merge_request_response
 
 merge_request_url=$(echo $created_merge_request_response | jq '.web_url' | tr -d '"')
