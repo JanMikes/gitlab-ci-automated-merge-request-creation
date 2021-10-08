a81ae62749bb740fc6932f51354b998e6844be60
Jan Mikeš <j.mikes@me.com>
2021-10-08 09:59:05 +0200

    Open merge request when not existing

--- open-merge-request.sh
+++ open-merge-request.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-set -ex
-
-jq --version
 
 # Need all remote branches
 git fetch --no-tags
@@ -12,15 +9,10 @@ git show-branch --remote
 nearest_parent_branch=$(git show-branch --remote | sed "s/].*//" | grep "++ \[" | head -n1 | sed "s/^.*\[origin\///")
 
 ## Debug statement
-echo $nearest_parent_branch
+echo "Nearest parent branch: ${nearest_parent_branch}"
 
-echo $CI_COMMIT_MESSAGE
 request_body=$(jq -c -r -n --arg title "$CI_COMMIT_MESSAGE" '{title: $title}')
-echo $request_body
-created_merge_request_response=$(curl -X POST --header "Authorization:Bearer ${PERSONAL_ACCESS_TOKEN}" --header "Content-Type:application/json" -d "$request_body" "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/merge_requests?source_branch=${CI_COMMIT_BRANCH}&target_branch=${nearest_parent_branch}")
-
-## Debug statement
-echo $created_merge_request_response
+created_merge_request_response=$(curl -X POST --header "Authorization:Bearer ${PERSONAL_ACCESS_TOKEN}" --header "Content-Type:application/json" -d "${request_body}" "https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/merge_requests?source_branch=${CI_COMMIT_BRANCH}&target_branch=${nearest_parent_branch}")
 
 merge_request_url=$(echo $created_merge_request_response | jq '.web_url' | tr -d '"')
 
